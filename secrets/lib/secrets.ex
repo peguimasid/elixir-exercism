aa47ae605abba7ef36c843ec144b423bade13c1b
Guilhermo Masid <guilhermomasid@gmail.com>
2026-03-10 18:27:45 -0300

    secrets

--- secrets/lib/secrets.ex
+++ secrets/lib/secrets.ex
@@ -1,29 +1,50 @@
 defmodule Secrets do
   def secret_add(secret) do
-    # Please implement the secret_add/1 function
+    # fn param ->
+    #   param + secret
+    # end
+    &(&1 + secret)
   end
 
   def secret_subtract(secret) do
-    # Please implement the secret_subtract/1 function
+    # fn param ->
+    #   param - secret
+    # end
+    &(&1 - secret)
   end
 
   def secret_multiply(secret) do
-    # Please implement the secret_multiply/1 function
+    # fn param ->
+    #   param * secret
+    # end
+    &(&1 * secret)
   end
 
   def secret_divide(secret) do
-    # Please implement the secret_divide/1 function
+    # fn param ->
+    #   div(param, secret)
+    # end
+    &div(&1, secret)
   end
 
   def secret_and(secret) do
-    # Please implement the secret_and/1 function
+    # fn param ->
+    #   Bitwise.band(param, secret)
+    # end
+    &Bitwise.band(&1, secret)
   end
 
   def secret_xor(secret) do
-    # Please implement the secret_xor/1 function
+    # fn param ->
+    #   Bitwise.band(param, secret)
+    # end
+    &Bitwise.bxor(&1, secret)
   end
 
   def secret_combine(secret_function1, secret_function2) do
-    # Please implement the secret_combine/2 function
+    # fn param ->
+    #   secret_function2.(secret_function1.(param))
+    # end
+    &secret_function2.(secret_function1.(&1))
   end
 end
