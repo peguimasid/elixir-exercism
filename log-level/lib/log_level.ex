9af4b0d4d296efd03bd483ebbd6b29e6cd68fd72
Guilhermo Masid <guilhermomasid@gmail.com>
2026-03-10 18:49:54 -0300

    finish log level

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,9 +1,22 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      level == 0 and not legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 and not legacy? -> :fatal
+      true -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    cond do
+      to_label(level, legacy?) in [:error, :fatal] -> :ops
+      to_label(level, legacy?) == :unknown and legacy? -> :dev1
+      to_label(level, legacy?) == :unknown -> :dev2
+      true -> false
+    end
   end
 end
