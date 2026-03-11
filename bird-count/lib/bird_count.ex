f4aec06e46fe14c7a2a73c16ff2d934c87b7a098
Guilhermo Masid <guilhermomasid@gmail.com>
2026-03-11 19:07:10 -0300

    alright it passes but im not supposed to use clever elixir, no Stream, Enum or List comprehensions

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -1,21 +1,13 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
-  end
+  def today(list), do: list |> Enum.at(0)
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
-  end
+  def increment_day_count([]), do: [1]
+  def increment_day_count([head | tail]), do: [head + 1 | tail]
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
-  end
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?(list), do: Enum.any?(list, &(&1 == 0))
 
-  def total(list) do
-    # Please implement the total/1 function
-  end
+  def total(list), do: Enum.sum(list)
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
-  end
+  def busy_days(list), do: Enum.count(list, &(&1 >= 5))
 end
