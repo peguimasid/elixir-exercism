24db65a2346b48bd8c53ae03888bd1b9be7c3a7d
Guilhermo Masid <guilhermomasid@gmail.com>
2026-03-16 19:17:57 -0300

    finish german-sysadmin

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -1,10 +1,31 @@
 defmodule Username do
   def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
+    username
+    |> remove_non_allowed_chars()
+    |> replace_german_chars()
+  end
 
-    # Please implement the sanitize/1 function
+  defp remove_non_allowed_chars(charlist) do
+    charlist
+    |> Enum.filter(&allowed_char?/1)
+  end
+
+  defp allowed_char?(char) when char in ?a..?z, do: true
+  defp allowed_char?(char) when char in [?ä, ?ö, ?ü, ?ß, ?_], do: true
+  defp allowed_char?(_char), do: false
+
+  defp replace_german_chars(charlist) do
+    charlist
+    |> Enum.flat_map(&replace_german_char/1)
+  end
+
+  defp replace_german_char(char) do
+    case char do
+      ?ö -> ~c"oe"
+      ?ä -> ~c"ae"
+      ?ü -> ~c"ue"
+      ?ß -> ~c"ss"
+      _ -> [char]
+    end
   end
 end
