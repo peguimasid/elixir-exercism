88f7b75484779709e31896d1808a614ce9072ac1
Guilhermo Masid <guilhermomasid@gmail.com>
2026-03-17 19:14:52 -0300

    finish take-a-number

--- take-a-number/lib/take_a_number.ex
+++ take-a-number/lib/take_a_number.ex
@@ -1,5 +1,24 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(&loop/0)
+  end
+
+  defp loop(current_number \\ 0) do
+    receive do
+      {:report_state, sender_id} ->
+        send(sender_id, current_number)
+        loop(current_number)
+
+      {:take_a_number, sender_id} ->
+        current_number = current_number + 1
+        send(sender_id, current_number)
+        loop(current_number)
+
+      :stop ->
+        Process.exit(self(), :normal)
+
+      _ ->
+        loop(current_number)
+    end
   end
 end
