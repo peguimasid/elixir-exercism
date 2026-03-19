c17e8eabd55a326b107661b5c7d06d7e39de1182
Guilhermo Masid <guilhermomasid@gmail.com>
2026-03-19 19:40:33 -0300

    finish library-fees

--- library-fees/lib/library_fees.ex
+++ library-fees/lib/library_fees.ex
@@ -1,25 +1,39 @@
 defmodule LibraryFees do
   def datetime_from_string(string) do
-    # Please implement the datetime_from_string/1 function
+    NaiveDateTime.from_iso8601!(string)
   end
 
   def before_noon?(datetime) do
-    # Please implement the before_noon?/1 function
+    datetime.hour < 12
   end
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+    days_to_add = if before_noon?(checkout_datetime), do: 28, else: 29
+    return_date = NaiveDateTime.add(checkout_datetime, days_to_add, :day)
+    NaiveDateTime.to_date(return_date)
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    # Please implement the days_late/2 function
+    diff = Date.diff(actual_return_datetime, planned_return_date)
+    max(diff, 0)
   end
 
   def monday?(datetime) do
-    # Please implement the monday?/1 function
+    Date.day_of_week(datetime) == 1
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    # Please implement the calculate_late_fee/3 function
+    checkout_date = datetime_from_string(checkout)
+    return_date = datetime_from_string(return)
+
+    planned_return_date = return_date(checkout_date)
+    days_late = days_late(planned_return_date, return_date)
+
+    fee = days_late * rate
+
+    case monday?(return_date) do
+      true -> Integer.floor_div(fee, 2)
+      false -> fee
+    end
   end
 end
