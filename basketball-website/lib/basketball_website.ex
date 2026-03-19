70ee6088e0407ad6651254e64401d47c83196011
Guilhermo Masid <guilhermomasid@gmail.com>
2026-03-19 20:09:24 -0300

    finish basketball-website

--- basketball-website/lib/basketball_website.ex
+++ basketball-website/lib/basketball_website.ex
@@ -1,9 +1,17 @@
 defmodule BasketballWebsite do
-  def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+  def extract_from_path(data, path) when is_binary(path) do
+    do_extract_from_path(data, String.split(path, "."))
+  end
+
+  defp do_extract_from_path(data, [head]) do
+    data[head]
+  end
+
+  defp do_extract_from_path(data, [head | tail]) do
+    do_extract_from_path(data[head], tail)
   end
 
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    get_in(data, String.split(path, "."))
   end
 end
