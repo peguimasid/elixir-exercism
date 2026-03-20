c9a8c897421b28b19c4b93fa87cd192682c3cc9f
Guilhermo Masid <guilhermomasid@gmail.com>
2026-03-20 18:52:20 -0300

    finish boutique-inventory/

--- boutique-inventory/lib/boutique_inventory.ex
+++ boutique-inventory/lib/boutique_inventory.ex
@@ -1,21 +1,27 @@
 defmodule BoutiqueInventory do
   def sort_by_price(inventory) do
-    # Please implement the sort_by_price/1 function
+    Enum.sort_by(inventory, & &1.price, :asc)
   end
 
   def with_missing_price(inventory) do
-    # Please implement the with_missing_price/1 function
+    Enum.filter(inventory, &(&1.price == nil))
   end
 
   def update_names(inventory, old_word, new_word) do
-    # Please implement the update_names/3 function
+    Enum.map(inventory, fn product ->
+      %{product | name: String.replace(product.name, old_word, new_word)}
+    end)
   end
 
   def increase_quantity(item, count) do
-    # Please implement the increase_quantity/2 function
+    quantity_by_size =
+      Map.new(item[:quantity_by_size], fn {key, value} -> {key, value + count} end)
+
+    %{item | quantity_by_size: quantity_by_size}
   end
 
   def total_quantity(item) do
-    # Please implement the total_quantity/1 function
+    item[:quantity_by_size]
+    |> Enum.reduce(0, fn {_key, val}, acc -> acc + val end)
   end
 end
