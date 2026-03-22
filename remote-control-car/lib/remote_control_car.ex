52f7799eede0a42a12416f14abefc1af56b0dc3d
Guilhermo Masid <guilhermomasid@gmail.com>
2026-03-22 16:51:06 -0300

    finish remote-control-car/

--- remote-control-car/lib/remote_control_car.ex
+++ remote-control-car/lib/remote_control_car.ex
@@ -1,23 +1,32 @@
 defmodule RemoteControlCar do
-  # Please implement the struct with the specified fields
+  @enforce_keys [:nickname]
+  defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
 
-  def new() do
-    # Please implement the new/0 function
+  def new(nickname \\ "none") do
+    %RemoteControlCar{nickname: nickname}
   end
 
-  def new(nickname) do
-    # Please implement the new/1 function
+  def display_distance(%RemoteControlCar{distance_driven_in_meters: distance}) do
+    "#{distance} meters"
   end
 
-  def display_distance(remote_car) do
-    # Please implement the display_distance/1 function
+  def display_battery(%RemoteControlCar{battery_percentage: 0}) do
+    "Battery empty"
   end
 
-  def display_battery(remote_car) do
-    # Please implement the display_battery/1 function
+  def display_battery(%RemoteControlCar{battery_percentage: battery_percentage}) do
+    "Battery at #{battery_percentage}%"
   end
 
-  def drive(remote_car) do
-    # Please implement the drive/1 function
+  def drive(%RemoteControlCar{battery_percentage: 0} = remote_car) do
+    remote_car
+  end
+
+  def drive(%RemoteControlCar{} = remote_car) do
+    %RemoteControlCar{
+      remote_car
+      | battery_percentage: remote_car.battery_percentage - 1,
+        distance_driven_in_meters: remote_car.distance_driven_in_meters + 20
+    }
   end
 end
