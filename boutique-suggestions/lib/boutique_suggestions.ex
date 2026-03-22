bcc05d1ff88bc2f5dd5a2c85fc6325d829432f25
Guilhermo Masid <guilhermomasid@gmail.com>
2026-03-22 17:45:52 -0300

    finish boutique-suggestions/

--- boutique-suggestions/lib/boutique_suggestions.ex
+++ boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,5 +1,12 @@
 defmodule BoutiqueSuggestions do
-  def get_combinations(tops, bottoms, options) do
-    # Please implement the get_combinations/3 function
+  def get_combinations(tops, bottoms, options \\ []) do
+    max_price = Keyword.get(options, :maximum_price, 100)
+
+    for top <- tops,
+        bottom <- bottoms,
+        top.base_color != bottom.base_color,
+        top.price + bottom.price <= max_price do
+      {top, bottom}
+    end
   end
 end
