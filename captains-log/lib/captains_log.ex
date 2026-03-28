c8d254fbbb2e2575493739656ecbf9fd96e691ed
Guilhermo Masid <guilhermomasid@gmail.com>
2026-03-28 19:20:06 -0300

    finish captains-log

--- captains-log/lib/captains_log.ex
+++ captains-log/lib/captains_log.ex
@@ -2,18 +2,26 @@ defmodule CaptainsLog do
   @planetary_classes ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
 
   def random_planet_class() do
-    # Please implement the random_planet_class/0 function
+    Enum.random(@planetary_classes)
   end
 
   def random_ship_registry_number() do
-    # Please implement the random_ship_registry_number/0 function
+    "NCC-#{Enum.random(1000..9999)}"
   end
 
   def random_stardate() do
-    # Please implement the random_stardate/0 function
+    min = 41000.0
+    max = 42000.0
+    min + :rand.uniform() * (max - min)
   end
 
-  def format_stardate(stardate) do
-    # Please implement the format_stardate/1 function
+  def format_stardate(stardate) when is_float(stardate) do
+    stardate
+    |> Float.round(1)
+    |> Float.to_string()
+  end
+
+  def format_stardate(_stardate) do
+    raise ArgumentError
   end
 end
