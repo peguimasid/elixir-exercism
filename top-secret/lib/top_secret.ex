bb1ec4367b03c791226989f8c50f892b5e265e92
Guilhermo Masid <guilhermomasid@gmail.com>
2026-04-12 16:42:08 -0300

    finish top-secret

--- top-secret/lib/top_secret.ex
+++ top-secret/lib/top_secret.ex
@@ -1,13 +1,36 @@
 defmodule TopSecret do
   def to_ast(string) do
-    # Please implement the to_ast/1 function
+    Code.string_to_quoted!(string)
   end
 
   def decode_secret_message_part(ast, acc) do
-    # Please implement the decode_secret_message_part/2 function
+    case check_op(ast) do
+      nil -> {ast, acc}
+      name -> {ast, [name | acc]}
+    end
   end
 
+  defp check_op({op, _metadata, [{:when, _, [{fun, _, args}, _guard]}, _body]})
+       when op in [:def, :defp] do
+    fun
+    |> Atom.to_string()
+    |> String.slice(0, length(args || []))
+  end
+
+  defp check_op({op, _metadata, [{fun, _, args}, _body]}) when op in [:def, :defp] do
+    fun
+    |> Atom.to_string()
+    |> String.slice(0, length(args || []))
+  end
+
+  defp check_op(_), do: nil
+
   def decode_secret_message(string) do
-    # Please implement the decode_secret_message/1 function
+    string
+    |> to_ast()
+    |> Macro.prewalk([], &decode_secret_message_part/2)
+    |> elem(1)
+    |> Enum.reverse()
+    |> Enum.join()
   end
 end
