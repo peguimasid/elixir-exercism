3494b1a7851621df1270309ac18f8e16b722b837
Guilhermo Masid <guilhermomasid@gmail.com>
2026-04-20 21:01:24 -0300

    feat: finish dancing dots

--- dancing-dots/lib/dancing_dots/animation.ex
+++ dancing-dots/lib/dancing_dots/animation.ex
@@ -1,16 +1,54 @@
 defmodule DancingDots.Animation do
+  alias DancingDots.Animation
   @type dot :: DancingDots.Dot.t()
   @type opts :: keyword
   @type error :: any
   @type frame_number :: pos_integer
 
-  # Please implement the module
+  @callback init(opts :: opts()) :: {:ok, opts()} | {:error, error()}
+  @callback handle_frame(dot :: dot(), frame_number :: frame_number(), opts :: opts()) :: dot()
+
+  defmacro __using__(_) do
+    quote do
+      @behaviour Animation
+      def init(opts), do: {:ok, opts}
+      defoverridable init: 1
+    end
+  end
 end
 
 defmodule DancingDots.Flicker do
-  # Please implement the module
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def handle_frame(%DancingDots.Dot{opacity: opacity} = dot, frame_number, _opts)
+      when rem(frame_number, 4) === 0 do
+    %DancingDots.Dot{dot | opacity: opacity / 2}
+  end
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, _frame_number, _opts), do: dot
 end
 
 defmodule DancingDots.Zoom do
-  # Please implement the module
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def init(opts) do
+    velocity = Keyword.get(opts, :velocity)
+
+    if is_number(velocity) do
+      {:ok, opts}
+    else
+      {:error,
+       "The :velocity option is required, and its value must be a number. Got: #{inspect(velocity)}"}
+    end
+  end
+
+  @impl DancingDots.Animation
+  def handle_frame(%DancingDots.Dot{radius: radius} = dot, frame_number, opts) do
+    velocity = Keyword.get(opts, :velocity)
+
+    %DancingDots.Dot{dot | radius: radius + (frame_number - 1) * velocity}
+  end
 end
