884e8ec98f76ce9f5930832288e40337a1ddc99a
Guilhermo Masid <guilhermomasid@gmail.com>
2026-04-27 19:58:58 -0300

    implement everything but auto shutdown

--- take-a-number-deluxe/lib/take_a_number_deluxe.ex
+++ take-a-number-deluxe/lib/take_a_number_deluxe.ex
@@ -1,32 +1,82 @@
 defmodule TakeANumberDeluxe do
   # Client API
+  use GenServer
+
+  alias TakeANumberDeluxe.State
 
   @spec start_link(keyword()) :: {:ok, pid()} | {:error, atom()}
   def start_link(init_arg) do
-    # Please implement the start_link/1 function
+    min = Keyword.get(init_arg, :min_number)
+    max = Keyword.get(init_arg, :max_number)
+    timeout = Keyword.get(init_arg, :auto_shutdown_timeout, :infinity)
+
+    # Use the State.new/3 function to validate and build the state struct
+    case State.new(min, max, timeout) do
+      {:ok, state} ->
+        GenServer.start_link(__MODULE__, state)
+
+      {:error, :invalid_configuration} ->
+        {:error, :invalid_configuration}
+    end
   end
 
   @spec report_state(pid()) :: TakeANumberDeluxe.State.t()
   def report_state(machine) do
-    # Please implement the report_state/1 function
+    GenServer.call(machine, :report_state)
   end
 
   @spec queue_new_number(pid()) :: {:ok, integer()} | {:error, atom()}
   def queue_new_number(machine) do
-    # Please implement the queue_new_number/1 function
+    GenServer.call(machine, :queue_new_number)
   end
 
   @spec serve_next_queued_number(pid(), integer() | nil) :: {:ok, integer()} | {:error, atom()}
   def serve_next_queued_number(machine, priority_number \\ nil) do
-    # Please implement the serve_next_queued_number/2 function
+    GenServer.call(machine, {:serve_next_queued_number, priority_number})
   end
 
   @spec reset_state(pid()) :: :ok
   def reset_state(machine) do
-    # Please implement the reset_state/1 function
+    GenServer.call(machine, :reset_state)
   end
 
   # Server callbacks
 
-  # Please implement the necessary callbacks
+  def init(init_arg) do
+    {:ok, init_arg}
+  end
+
+  def handle_call(:report_state, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call(:queue_new_number, _from, state) do
+    case State.queue_new_number(state) do
+      {:ok, new_number, new_state} ->
+        {:reply, {:ok, new_number}, new_state}
+
+      {:error, reason} ->
+        {:reply, {:error, reason}, state}
+    end
+  end
+
+  def handle_call({:serve_next_queued_number, priority_number}, _from, state) do
+    case State.serve_next_queued_number(state, priority_number) do
+      {:ok, new_number, new_state} ->
+        {:reply, {:ok, new_number}, new_state}
+
+      {:error, reason} ->
+        {:reply, {:error, reason}, state}
+    end
+  end
+
+  def handle_call(:reset_state, _from, state) do
+    min = state.min_number
+    max = state.max_number
+    timeout = state.auto_shutdown_timeout
+
+    {:ok, state} = State.new(min, max, timeout)
+
+    {:reply, :ok, state}
+  end
 end
